020f6e209bc1a0930f0f69d23579a79fccf80d07
jdelia1 <lab@wm320-a229565.campus.ithaca.lan>
2015-04-02 20:37:54 -0400

    TableViewController swift file

---
+++ chemCat/CatalogViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CatalogViewController.swift
+//  chemCat
+//
+//  Created by lab on 4/2/15.
+//  Copyright (c) 2015 Mayo Industries. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CatalogViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
